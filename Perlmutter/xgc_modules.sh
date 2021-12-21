c0e01d731ab62cd833ce6082bcadf702dd0482d4
Chonglin Zhang <zhangchonglin@gmail.com>
2021-12-21 14:33:02 -0800

    add module script for Perlmutter

---
+++ Perlmutter/xgc_modules.sh
@@ -0,0 +1,7 @@
+module load PrgEnv-gnu
+module load systemlayer
+module load cuda/11.3.1
+module load cpe-cuda
+module load cmake/3.22.0
+module load valgrind4hpc/2.12.4
+module load cray-fftw/3.3.8.12
