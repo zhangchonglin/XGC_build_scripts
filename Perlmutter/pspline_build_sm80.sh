39ea947605f10e7849dc1b127ab6a200c5286c4e
Chonglin Zhang <zhangchonglin@gmail.com>
2021-12-21 14:33:47 -0800

    add pspline build script for Perlmutter

---
+++ Perlmutter/pspline_build_sm80.sh
@@ -0,0 +1,30 @@
+module load PrgEnv-gnu
+module load systemlayer
+module load cuda/11.3.1
+module load cpe-cuda
+module load cmake/3.22.0
+module load valgrind4hpc/2.12.4
+module load cray-fftw/3.3.8.12
+
+export installroot=$PWD
+export srcroot=$installroot/../XGC-Devel/libs
+
+# pspline
+export pspline_src=$srcroot/pspline
+export pspline_install_root=$installroot/pspline/install
+
+cd $installroot
+mkdir -p pspline/build
+cd pspline/build
+
+cmake $pspline_src \
+      -DCMAKE_C_COMPILER="cc" \
+      -DCMAKE_CXX_COMPILER="CC" \
+      -DCMAKE_Fortran_COMPILER="ftn" \
+      -DCMAKE_C_FLAGS="-g -O3" \
+      -DCMAKE_CXX_FLAGS="-g -O3" \
+      -DCMAKE_Fortran_FLAGS="-fpic -ffree-line-length-0"\
+      -DCMAKE_INSTALL_PREFIX=$pspline_install_root
+      -DCMAKE_BUILD_TYPE=Release
+
+make -j4 install
