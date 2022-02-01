3b99e0b64327bf9d59af39d9833ef043d0a170b0
Chonglin Zhang <zhangchonglin@gmail.com>
2022-02-01 17:22:48 -0500

    update CentOS7 build script for XGC

--- CentOS/xgc_build_sm61.sh
+++ CentOS/xgc_build_sm61.sh
@@ -32,7 +32,10 @@ cd xgc/build
 
 export XGC_PLATFORM=centos7
 cmake $xgc_src_root \
-      -DCMAKE_BUILD_TYPE=Debug -DXGC1=ON \
-      -DDELTAF_CONV=ON
+      -DCMAKE_BUILD_TYPE=Debug -DDELTAF_CONV=ON \
+      -DCMAKE_Fortran_COMPILER="gfortran" \
+      -DCMAKE_C_FLAGS="-fopenmp" \
+      -DCMAKE_CXX_FLAGS="-fopenmp" \
+      -DCMAKE_Fortran_FLAGS="-fopenmp"
 
 make -j4 xgc-es-cpp xgc-es-cpp-gpu
